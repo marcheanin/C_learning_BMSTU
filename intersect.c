31ad50b56af3a98b4f1347e58f43d09369c50e9a
marcheanin <marcheanin1576@gmail.com>
2021-11-01 12:30:26 +0300

    finish intersect.c

---
+++ intersect.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    long a = 0, b = 0;
+    int n, m, x;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; i++){
+        scanf("%d", &x);
+        a += pow(2, x);
+    }
+    for (int i = 0; i < m; i++){
+        scanf("%d", &x);
+        b += pow(2, x);
+    }
+    int s[32];
+    for (int i = 0; i < 32; i++) s[i] = 0;
+    for (int i = 31; i >= 0; i--){
+        if (a >= pow(2, i) && b >= pow(2, i)){
+            s[i] = 1;
+            a -= pow(2, i);
+            b -= pow(2, i);
+        }
+        else if (a >= pow(2, i)) a -= pow(2, i);
+        else if (b >= pow(2, i)) b -= pow(2, i);
+    }
+    for (int i = 0; i < 32; i++){
+        if (s[i] == 1){
+            printf("%d ", i);
+        }
+    }
+}
