683b31554c32c88511f404c41e042ef94d0e1e04
marcheanin <marcheanin1576@gmail.com>
2021-11-01 23:56:49 +0300

    finish task#7

---
+++ primediv.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    long x;
+    scanf("%ld", &x);
+    long n = abs(x);
+    x = abs(x);
+    for (int i = 2; i <= n; i++){
+        if (i == x){
+            printf("%ld", x);
+            return 0;
+        }
+        if (x % i == 0){
+            x /= i;
+            i--;
+        }
+    }
+}
