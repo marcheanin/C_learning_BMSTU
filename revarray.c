298891c8f48b1527aef6cecbfb10216d1967cd63
marcheanin <marcheanin1576@gmail.com>
2021-11-03 00:43:44 +0300

    finish #9 task

---
+++ revarray.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+void revarray(void *base, size_t nel, size_t width){
+    for (void *i = base, *j =  base + (nel - 1) * width; i < j; i += width, j -= width){
+        int x = *(char*)i;
+        *(char*)i = *(char*)j;
+        *(char*)j = x;
+    }
+}
+int main(){
+    int s[] = {1, 2, 3, 17, 4, 5, 6};
+    int *a = s, n = 7;
+    revarray(a, n, sizeof(int));
+    for (int i = 0; i < 7; i++){
+        printf("%d ", s[i]);
+    }
+
+}
