10ff1a3ec47db3668333a92fed3dc3f342f628af
marcheanin <marcheanin1576@gmail.com>
2021-11-08 08:24:40 +0300

    finish #14 task

---
+++ wcount.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int wcount(char *s){
+    int ans = 0;
+    for (int i = 0; i < strlen(s); i++){
+        if (i == 0 && s[i] != ' ' || (s[i] != ' ' && s[i - 1] == ' ')){
+            ans++;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    char s[100];
+    gets(s);
+    printf("%s\n", s);
+    printf("%d", wcount(s));
+}
